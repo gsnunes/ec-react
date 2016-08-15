5189d1136dd5668d1942d3674f9ffe48dfa95a0b
Guilherme Nunes <guinunesilva@gmail.com>
2016-08-15 10:48:00 -0300

    change to mocha and chai

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['browserify', 'jasmine'],
+    frameworks: ['browserify', 'mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
       './dist/bundle.js',
-      './test/**/*Spec.js',
+      './test/**/*.spec.js',
     ],
 
 
@@ -28,7 +28,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      './test/**/*Spec.js': ['browserify'],
+      './test/**/*.spec.js': ['browserify'],
     },
 
 
@@ -41,7 +41,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
---
+++ test/Hello.spec.js
@@ -0,0 +1,6 @@
+describe('example test', () => {
+  it('should be fail', () => {
+    /* eslint-disable no-unused-expressions */
+    expect(true).to.be.false;
+  });
+});
